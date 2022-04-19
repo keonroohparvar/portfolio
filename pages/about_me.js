a1ca8c168cf66bc7ba5e7dc862617466fc856e19
Keon <kroohpar@gmail.com>
2022-04-19 15:08:05 -0700

    Intitial work with pages

---
+++ pages/about_me.js
@@ -0,0 +1,11 @@
+// Multiple Pages: https://www.techomoro.com/how-to-create-a-multi-page-website-with-next-js/
+
+import React from "react";
+import styles from "../styles/about_me.module.css";
+
+
+export default function AboutMe() {
+    return (
+        <h1>TEXT</h1>
+    )
+}
---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+// Multiple Pages: https://www.techomoro.com/how-to-create-a-multi-page-website-with-next-js/
+
+import styles from '../styles/Home.module.css'
+
+const Projects = () => {
+    return (
+        <h1>TEXT</h1>
+    )
+}
+
+export default Projects
